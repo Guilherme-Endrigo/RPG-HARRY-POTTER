6340aaafc7d4dd379039e58a42a7363ddb87414f
Guilherme_Endrigo <endrigo.guilherme@hotmail.com>
2021-07-30 22:39:16 -0300

    criação projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
